4412ce89758563fd5baf9282353e79a02ab36124
23040526 <23040526@myrp.edu.sg>
2024-07-29 10:44:45 +0800

    -m

--- app.js
+++ app.js
@@ -439,6 +439,3 @@ app.get('/deleteProduct/:id', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000
-;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
